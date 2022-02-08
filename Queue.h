bd67d68d49a53777c838083863ac2be20d4dd6a9
Oz-Guzman <75756076+Oz-Guzman@users.noreply.github.com>
2022-02-07 23:16:17 -0600

    Update Queue.h

--- Queue.h
+++ Queue.h
@@ -5,18 +5,21 @@ class Queue
 public:
     bool isEmpty() const
     {
-        return true;
+        return theList.empty();
     }
     const Object& getFront() const
     {
-        return 0;
+        return theList.front();
     }
     void enqueue(const Object& x)
     {
+        theList.push_back(x);
     }
     Object& dequeue()
     {
-        return *theList.begin();
+        Object temp = theList.front();
+        theList.pop_front();
+        return temp;
     }
 private:
     List<Object> theList;
